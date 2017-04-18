daa51060f922b493d766ca1a55a3db5c52bc681a
galihpw <galih1995@gmail.com>
2017-04-19 06:33:28 +0700

    Modul Saku Agrin

---
+++ app/src/main/java/com/galihpw/projectmodul/IsiMenu1.java
@@ -0,0 +1,13 @@
+package com.galihpw.projectmodul;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IsiMenu1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isi_menu1);
+    }
+}
---
+++ app/src/main/java/com/galihpw/projectmodul/MainActivity.java
@@ -0,0 +1,31 @@
+package com.galihpw.projectmodul;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by GalihPW on 18/04/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bStart = (Button) findViewById(R.id.bStart);
+
+        bStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
