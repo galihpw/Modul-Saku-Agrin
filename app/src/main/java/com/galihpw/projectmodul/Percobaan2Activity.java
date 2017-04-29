1e8af82a31b38c7851cae871cf1cb5586370d989
azizashari <aziz.ashari1@gmail.com>
2017-04-30 06:26:09 +0700

    Eleventh - Sub Menu Percobaan 2 & 3

---
+++ app/src/main/java/com/galihpw/projectmodul/Percobaan2Activity.java
@@ -0,0 +1,36 @@
+package com.galihpw.projectmodul;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.galihpw.projectmodul.Percobaan2.IsiPercobaan2;
+import com.galihpw.projectmodul.Percobaan4.IsiPercobaan4a;
+
+public class Percobaan2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percobaan2);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        //Menu 1
+        Button bMenu1 = (Button) findViewById(R.id.bMenu1);
+
+        bMenu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Percobaan2Activity.this,IsiPercobaan2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
