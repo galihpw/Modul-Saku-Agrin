e8f0501b3f44ae8499b6b71fa908a250ae050647
azizashari <aziz.ashari1@gmail.com>
2017-05-02 07:14:14 +0700

    TABEL FIXED

--- app/src/main/java/com/galihpw/projectmodul/Percobaan2Activity.java
+++ app/src/main/java/com/galihpw/projectmodul/Percobaan2Activity.java
@@ -1,14 +1,13 @@
 package com.galihpw.projectmodul;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.galihpw.projectmodul.Percobaan2.IsiPercobaan2;
-import com.galihpw.projectmodul.Percobaan4.IsiPercobaan4a;
 
 public class Percobaan2Activity extends AppCompatActivity {
 
