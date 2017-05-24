bfe5421193cf77f07ca71d2859cbbe96def18ae6
galihpw <galih1995@gmail.com>
2017-05-24 07:55:58 +0700

    Revisi v1

--- app/src/main/java/com/galihpw/projectmodul/MainActivity.java
+++ app/src/main/java/com/galihpw/projectmodul/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * Created by GalihPW on 18/04/2017.
@@ -16,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.mptp);
+
+        Glide.with(this).load(R.drawable.mptp).into(imageView);
 
         Button bStart = (Button) findViewById(R.id.bStart);
 
